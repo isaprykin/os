29e9013e16988ea2ef6082c05f8db3f8294376ec
Igor Saprykin <sheleztt@gmail.com>
2020-09-18 22:02:52 -0700

    Untabify the NASM files for greater standardization.
    For some reason Emacs' nasm-mode I found put tabs in.  (One could add
    `(setq-default indent-tabs-mode nil)` to their configuration to be
    sure for the future)

--- boot-sect.asm
+++ boot-sect.asm
@@ -1,19 +1,19 @@
 [org 0x7c00]
 
-	mov bx, 0x1000 		; The memory offest where to place the kernel.
-	mov dh, 15		; Read 15*512=7680 bytes.
-	mov dl, 0		; The boot drive.
-	call load_disk_starting_second_sector
+        mov bx, 0x1000          ; The memory offest where to place the kernel.
+        mov dh, 15              ; Read 15*512=7680 bytes.
+        mov dl, 0               ; The boot drive.
+        call load_disk_starting_second_sector
 
-	call 0x1000		; The kernel got loaded to the address 1000h.
+        call 0x1000             ; The kernel got loaded to the address 1000h.
 loop:
-	jmp loop		; Jump in the sample place forever.
+        jmp loop                ; Jump in the sample place forever.
 
 %include 'read-disk.asm'
 
-	; The boot program has to be 512 bytes. NASM syntax here is:
-	; - $ is the address of current position
-	; - $$ is the address of the beginning of the current segment
-	times 510-($-$$) db 0	; Pad till the 510th byte.
+        ; The boot program has to be 512 bytes. NASM syntax here is:
+        ; - $ is the address of current position
+        ; - $$ is the address of the beginning of the current segment
+        times 510-($-$$) db 0   ; Pad till the 510th byte.
 
-	dw 0xaa55		; Magic value to indicate "bootable".
+        dw 0xaa55               ; Magic value to indicate "bootable".
