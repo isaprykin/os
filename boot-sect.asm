de4c9c1906623789873075d6e633c57ad3999836
isaprykin <sheleztt@gmail.com>
2020-09-25 23:35:48 -0700

    Switch to the 32-bit protected mode.
    This is done by configuring the Global Descriptor Table and setting
    the control register in NASM.  I tried to arrange for it to happen in
    the C layer, but there is no way to produce 16-bit code from GCC.  I
    verified that its 32-bit output is not backward compatible even with
    additional flags such as `-m16`.
    
    The GDT sets up two simple segments one for the code and one for the data.
    I verified that after this set up, the normal 32-bit code generated
    by GCC works correctly.

--- boot-sect.asm
+++ boot-sect.asm
@@ -1,15 +1,20 @@
 [org 0x7c00]
+KERNEL_ADDRESS equ 0x1000
 
-        mov bx, 0x1000          ; The memory offest where to place the kernel.
+        mov bx, KERNEL_ADDRESS  ; The memory offest where to place the kernel.
         mov dh, 15              ; Read 15*512=7680 bytes.
         mov dl, 0               ; The boot drive.
         call load_disk_starting_second_sector
 
-        call 0x1000             ; The kernel got loaded to the address 1000h.
-loop:
-        jmp loop                ; Jump in the sample place forever.
+	call switch_to_protected_mode
+
+[bits 32]
+protected_mode:
+        call KERNEL_ADDRESS	; We will not return.
+        jmp $	                ; Jump in the same place forever.
 
 %include 'read-disk.asm'
+%include 'switch-to-32-protected-mode.asm'
 
         ; The boot program has to be 512 bytes. NASM syntax here is:
         ; - $ is the address of current position
