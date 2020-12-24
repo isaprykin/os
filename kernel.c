6a6f1ba6bd07af2c75eee7ed9e0bd50f465fbc0a
Igor Saprykin <sheleztt@gmail.com>
2020-12-23 22:48:46 -0800

    Eliminate `int_types.h` with custom data types.
    No need to have custom data types when the equivalent types are
    available in `stdint.h`.  It is available even in the current
    cross-compiler even with the `-freestanding` mode.

--- kernel.c
+++ kernel.c
@@ -1,20 +1,20 @@
-#include "int-types.h"
+#include <stdint.h>
 
 struct idt_interrupt_gate {
-   u16int base_low;  // The lower 16 bits of the address to jump to when this interrupt fires.
-   u16int segment_selector;  // Segment Selector for destination code segment.
-   u8int  always_zero;
+   uint16_t base_low;  // The lower 16 bits of the address to jump to when this interrupt fires.
+   uint16_t segment_selector;  // Segment Selector for destination code segment.
+   uint8_t  always_zero;
    // Flags: |P|DPL|DPL|0|D|1|1|1:
    //  -   P: Segment Present flag
    //  - DPL: Descriptor privelege level
    //  -   D: Size of gate: 1 = 32 bits; 0 = 16 bits
-   u8int  flags;               
-   u16int base_high;  // The upper 16 bits of the address to jump to.
+   uint8_t  flags;               
+   uint16_t base_high;  // The upper 16 bits of the address to jump to.
 } __attribute__((packed));
 
 struct idt_descriptor {
-   u16int limit;
-   u32int base;
+   uint16_t limit;
+   uint32_t base;
 } __attribute__((packed));
 
 extern void isr0();
@@ -24,7 +24,7 @@ extern void idt_flush(u32int);
 struct idt_interrupt_gate idt_entries[2];
 struct idt_descriptor idt_descriptor_ptr;
 
-void idt_set_gate(u8int num, u32int base, u16int selector, u8int flags) {
+void idt_set_gate(uint8_t num, uint32_t base, uint16_t selector, uint8_t flags) {
    idt_entries[num].base_low = base & 0xFFFF;
    idt_entries[num].base_high = (base >> 16) & 0xFFFF;
 
@@ -35,16 +35,16 @@ void idt_set_gate(u8int num, u32int base, u16int selector, u8int flags) {
 
 void init_idt() {
    idt_descriptor_ptr.limit = sizeof(struct idt_interrupt_gate) * 2;
-   idt_descriptor_ptr.base  = (u32int) &idt_entries;  
+   idt_descriptor_ptr.base  = (uint32_t) &idt_entries;  
 
    // 0x08 bytes is the offset into code segment in GDT that skips over the 
    // null segment.
    // 0x8E is 8 for Present 0 privelege level in |P|DPL|DPL|0 and 
    // E for 80386 32-bit interrupt gate type.
-   idt_set_gate(0, (u32int)isr0, 0x08, 0x8E);
-   idt_set_gate(1, (u32int)isr1, 0x08, 0x8E);
+   idt_set_gate(0, (uint32_t)isr0, 0x08, 0x8E);
+   idt_set_gate(1, (uint32_t)isr1, 0x08, 0x8E);
 
-   idt_flush((u32int) &idt_descriptor_ptr);
+   idt_flush((uint32_t) &idt_descriptor_ptr);
 }
 
 int kernel_main() {
