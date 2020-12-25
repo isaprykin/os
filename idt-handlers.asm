71c2f8535855c33500e5ce735b056ca39af626e8
Igor Saprykin <sheleztt@gmail.com>
2020-12-24 21:18:17 -0800

    Complete the 32 interrupt handlers.
    This defines the system handlers.  Handlers after the 32nd are
    user-definable.

--- idt-handlers.asm
+++ idt-handlers.asm
@@ -1,18 +1,54 @@
 [EXTERN isr_handler]
 
-[GLOBAL isr0]
-isr0:
-  cli
-  push byte 0   ; A dummy error code
-  push byte 0   ; The interrupt number.
-  jmp isr_common_stub
+%macro ISR_NOERRCODE 1  ; define a macro, taking one parameter
+  [GLOBAL isr%1]        ; %1 accesses the first parameter.
+  isr%1:
+    cli
+    push byte 0         ; A dummy error code
+    push %1             ; The interrupt number.
+    jmp isr_common_stub
+%endmacro
 
-[GLOBAL isr1]
-isr1:
-  cli
-  push byte 0
-  push byte 1
-  jmp isr_common_stub
+%macro ISR_ERRCODE 1
+  [GLOBAL isr%1]
+  isr%1:
+    cli
+    push %1            ; The interrupt number.
+    jmp isr_common_stub
+%endmacro
+
+ISR_NOERRCODE 0
+ISR_NOERRCODE 1
+ISR_NOERRCODE 2
+ISR_NOERRCODE 3
+ISR_NOERRCODE 4
+ISR_NOERRCODE 5
+ISR_NOERRCODE 6
+ISR_NOERRCODE 7
+ISR_ERRCODE   8
+ISR_NOERRCODE 9
+ISR_ERRCODE   10
+ISR_ERRCODE   11
+ISR_ERRCODE   12
+ISR_ERRCODE   13
+ISR_ERRCODE   14
+ISR_NOERRCODE 15
+ISR_NOERRCODE 16
+ISR_ERRCODE   17
+ISR_NOERRCODE 18
+ISR_NOERRCODE 19
+ISR_NOERRCODE 20
+ISR_NOERRCODE 21
+ISR_NOERRCODE 22
+ISR_NOERRCODE 23
+ISR_NOERRCODE 24
+ISR_NOERRCODE 25
+ISR_NOERRCODE 26
+ISR_NOERRCODE 27
+ISR_NOERRCODE 28
+ISR_NOERRCODE 29
+ISR_ERRCODE   30
+ISR_NOERRCODE 31
 
 isr_common_stub:
    pushad
